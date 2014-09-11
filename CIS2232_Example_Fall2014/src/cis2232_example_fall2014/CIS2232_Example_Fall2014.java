cf528930cd0ff871bb037fa110e35d2c31c29099
BJ MacLean <bjmaclean@hollandcollege.com>
2014-09-11 13:31:34 -0300

    output statement
    test

--- CIS2232_Example_Fall2014/src/cis2232_example_fall2014/CIS2232_Example_Fall2014.java
+++ CIS2232_Example_Fall2014/src/cis2232_example_fall2014/CIS2232_Example_Fall2014.java
@@ -16,7 +16,7 @@ public class CIS2232_Example_Fall2014 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("This is working");
     }
     
 }
