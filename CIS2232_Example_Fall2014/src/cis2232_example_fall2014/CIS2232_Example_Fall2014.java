506df80164b778000a0efca14da17821a29a02f5
BJ MacLean <bjmaclean@hollandcollege.com>
2014-09-12 09:45:56 -0300

    removing again...

--- CIS2232_Example_Fall2014/src/cis2232_example_fall2014/CIS2232_Example_Fall2014.java
+++ CIS2232_Example_Fall2014/src/cis2232_example_fall2014/CIS2232_Example_Fall2014.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cis2232_example_fall2014;
 
 import java.io.BufferedWriter;
@@ -26,33 +25,6 @@ public class CIS2232_Example_Fall2014 {
      */
     public static void main(String[] args) {
         int todaysPin = Pin.getTodaysPin("CIS2232");
-        
-        System.out.println("Todays pin is "+todaysPin);
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("Enter your userid");        
-        String userId = input.nextLine();
-        
-        System.out.println("Enter the pin");        
-        String inPin = input.nextLine();
-        
 
-
-        
-        if (Integer.parseInt(inPin) == todaysPin) {
-            try {
-                //success
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("c:\\cis2232\\attendance.txt", true));                
-                
-                bufferedWriter.write(userId);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                //osw = new OutputStreamWriter();
-            } catch (IOException ex) {
-                Logger.getLogger(CIS2232_Example_Fall2014.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-                    
-        
     }
-    }}
+}
