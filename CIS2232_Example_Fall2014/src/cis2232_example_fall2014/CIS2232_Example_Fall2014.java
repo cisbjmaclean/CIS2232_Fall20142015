c9a099db6095d9231a88031dcfe940898b3af5da
BJ MacLean <bjmaclean@hollandcollege.com>
2014-09-12 10:38:25 -0300

    Summerside changes

--- CIS2232_Example_Fall2014/src/cis2232_example_fall2014/CIS2232_Example_Fall2014.java
+++ CIS2232_Example_Fall2014/src/cis2232_example_fall2014/CIS2232_Example_Fall2014.java
@@ -6,6 +6,7 @@
 package cis2232_example_fall2014;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -24,7 +25,36 @@ public class CIS2232_Example_Fall2014 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int todaysPin = Pin.getTodaysPin("CIS2232");
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter your Name");
+        String userName = input.nextLine();
+        System.out.println("Please Enter your Course ID");
+        String courseID = input.nextLine();
+        int todaysPin = Pin.getTodaysPin(courseID);
+        System.out.println("Please Enter your Todays Pin");
+        int inputTodaysPin = input.nextInt();
+
+        if (todaysPin == inputTodaysPin) {
+            System.out.println("Pin is Correct");
+
+            File file = new File("c:\\CIS2232\\add.txt");
+            try {
+                file.createNewFile();
+                BufferedWriter writer;
+                writer = new BufferedWriter(new FileWriter(file));
+                writer.write(userName);
+                writer.newLine();   
+                writer.write(courseID);
+                writer.newLine();                
+                writer.flush();
+                writer.close();
+            } catch (IOException ex) {
+                Logger.getLogger(CIS2232_Example_Fall2014.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        } else {
+            System.out.println("Pin is  in  Correct");
+        }
     }
 }
